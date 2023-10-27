02ed3291a2b451f31a0c15bc89e36c57daf36f2a
ana-capistrano <ana.eaabna@gmail.com>
2023-10-27 14:12:24 -0300

    ajustes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { TaskFormComponent } from './components/task-form/task-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { CurrencyFormatPipe } from './pipes/currency-format.pipe';
+import { TaskDetailComponent } from './components/task-detail/task-detail.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DateGreaterThanTodayDirective } from './directives/date-greater-than-today.directive';
+import { TaskFiltroComponent } from './components/task-filtro/task-filtro.component';
+import { TaskFormReactiveComponent } from './components/task-form-reactive/task-form-reactive.component';
+import { DateMaskDirective } from './directives/date-mask.directive';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskFormComponent,
+    TaskListComponent,
+    CurrencyFormatPipe,
+    TaskDetailComponent,
+    DateGreaterThanTodayDirective,
+    TaskFiltroComponent,
+    TaskFormReactiveComponent,
+    DateMaskDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/task-form/task-form.component.ts
+++ src/app/components/task-form/task-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Task } from 'src/models/task.model';
 
 @Component({
@@ -11,7 +12,11 @@ export class TaskFormComponent {
 
   public newTask = new Task();
 
-  submitTask() {
+  submitTask(form: NgForm) {
+    console.log('chamou');
+    if(!form.valid) return;
+    
+    console.log('chamou 02');
     this.addTask.emit(this.newTask);
     this.newTask = new Task();
   }
